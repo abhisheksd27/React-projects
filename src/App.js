5ab84ab42b12e0ed650686272a8bb0a501b46abc
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-03-29 16:31:55 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    if (!localStorage.getItem('username')) return <LoginForm />;
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="9babeaed-81aa-454b-9a4a-4c16b1898a8f"
+            userName="Abhishek"
+            userSecret="Abhishek@8861"
+            renderChatFeed={(chatAppProps)=><ChatFeed{ ... chatAppProps} />}
+            onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+            
+            />
+    )
 }
-
 export default App;
